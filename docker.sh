3a8de8eed859663ccaf2df3907861da573b847fc
Sophien Ababssi <abasop1@gmail.com>
2023-07-13 11:00:59 +0200

    adding wait database on docker.sh to fix issue first launch test

--- docker.sh
+++ docker.sh
@@ -1,4 +1,8 @@
-sleep 3
+# Attente pour la base de données pour être prête
+until php bin/console doctrine:query:sql "SELECT 1" >/dev/null 2>&1; do
+    echo "En attente pour la base de données..."
+    sleep 1
+done
 php bin/console doctrine:database:create --env=test
 php bin/console doctrine:migrations:migrate --env=test
 php bin/console d:m:m --no-interaction
